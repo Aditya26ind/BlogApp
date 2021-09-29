a262914f1acb9f962a0172bfd2b9ed30b15f14de
adityasalabh <adi0699sal@gmail.com>
2021-09-29 12:28:07 +0530

    changes

--- blognews/settings.py
+++ blognews/settings.py
@@ -164,5 +164,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT='587'
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='adi0699sal@gmail.com'
+EMAIL_HOST_USER='herokuheroku300@gmail.com'
 EMAIL_HOST_PASSWORD=config('KEY')
