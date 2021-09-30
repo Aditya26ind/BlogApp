9ae4551a0271023a31823a2b239b32ef3d122e7e
adityasalabh <adi0699sal@gmail.com>
2021-09-30 14:16:31 +0530

    changes

--- News/views.py
+++ News/views.py
@@ -7,9 +7,9 @@ from django.contrib.auth.decorators import login_required
 def homeview(request):
     if request.method=="POST":
         searched=request.POST['search']
-        data=requests.get(f"https://newsapi.org/v2/everything?q={searched}&from=2021-08-28&sortBy=publishedAt&apiKey=680e06c9e1be4252acdb6b31b289a09a").json()
+        data=requests.get(f"https://newsapi.org/v2/everything?q={searched}&from=2021-08-30&sortBy=publishedAt&apiKey=680e06c9e1be4252acdb6b31b289a09a").json()
         return render(request,'home.html',{'data':data})
     else:
-        data=requests.get("https://newsapi.org/v2/everything?q=tesla&from=2021-08-28&sortBy=publishedAt&apiKey=680e06c9e1be4252acdb6b31b289a09a").json()
+        data=requests.get("https://newsapi.org/v2/everything?q=tesla&from=2021-08-30&sortBy=publishedAt&apiKey=680e06c9e1be4252acdb6b31b289a09a").json()
         return render(request,'home.html',{'data':data})
 
