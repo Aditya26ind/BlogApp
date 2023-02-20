3902e365c4b2a936dcc13a79eb0d90f000ab8b2a
adityasalabh <adi0699sal@gmail.com>
2023-02-20 14:35:40 +0530

    Deployed settings

--- blognews/settings.py
+++ blognews/settings.py
@@ -87,14 +87,22 @@ WSGI_APPLICATION = 'blognews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':"blognews@database",
+        'HOST': "db.mlyufwnqnijnmrzplpoh.supabase.co",
+        'PORT': '5432',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
-import dj_database_url
-db_from_env=dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-django_heroku.settings(locals())
+# import dj_database_url
+# db_from_env=dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
+# django_heroku.settings(locals())
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
